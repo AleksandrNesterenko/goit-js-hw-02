c4401048967f17b13ec8adc8d4bfb24ffbdf38dc
Aleksandr <alex1989nesterenko@gmail.com>
2021-01-14 06:29:49 +0200

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -9,18 +9,45 @@
 // если массив не пустой, необходимо посчитать сумму всех элементов массива и записать ее в переменную total.
 // Используй цикл for или for...of.После чего в консоль выведи строку 'Общая сумма чисел равна [сумма]'.
 
+// let input;
+// const numbers = [];
+// let total = 0;
+
+// do {
+//   input = prompt('Введите Ваши цифры');
+//   if (input === null) {
+//     break;
+//   }
+
+//   input = Number(input);
+//   total = total + input;
+// } while (true);
+
+// console.log(`Общая сумма чисел равна ${total}`);
+
 let input;
 const numbers = [];
-let total = 0;
+let total = 0; 
 
-do {
-  input = prompt('Введите Ваши цифры');
-  if (input === null) {
-    break;
-  }
 
-  input = Number(input);
-  total = total + input;
-} while (true);
+while (input !== null) {
 
-console.log(`Общая сумма чисел равна ${total}`);
+    input = prompt('ввидите цифру');
+
+    if (Number.isNaN(Number(input))) {
+        alert('Было введено не число, попробуйте еще раз');
+        continue;
+    } 
+
+    if (input === null) {
+        console.log(`Общая сумма чисел равна ${total}`);
+        continue;
+    } 
+    numbers.push(Number(input));
+    console.log(numbers);
+
+    for(const number of numbers) {
+        total = total + number;
+        }
+    
+}
